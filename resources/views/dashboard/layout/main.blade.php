2a559e5f991565594be0823cbda56fff4e74d15b
adamjatim <adamrahmatilahi@gmail.com>
2024-06-30 04:32:19 +0700

    update: changes depedence

--- resources/views/dashboard/layout/main.blade.php
+++ resources/views/dashboard/layout/main.blade.php
@@ -6,40 +6,45 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
   <meta name="description" content="" />
   <meta name="author" content="" />
-  <title>Dashboard - Admin Kelurahan Ampel</title>
+  <title>Dashboard - {{ $title }}</title>
   <link rel="icon" type="image/x-icon" href="{{ secure_asset('admin/assets/favicon.ico') }}" />
   <link rel="icon" type="image/x-icon" href="{{ asset('admin/assets/favicon.ico') }}" />
-  <!-- Core theme CSS -->
-  {{-- <link rel="stylesheet" href="{{ asset('admin/css/styles.css') }}"> --}}
-  {{-- <link rel="stylesheet" href="{{ secure_asset('admin/css/styles.css') }}"> --}}
-  <!-- Core theme CSS (Tailwind)-->
+  {{-- Core theme CSS --}}
+  <link rel="stylesheet" href="{{ asset('admin/css/styles.css') }}">
+  <link rel="stylesheet" href="{{ secure_asset('admin/css/styles.css') }}">
+  {{-- Core theme CSS (Tailwind) --}}
   <script src="https://cdn.tailwindcss.com"></script>
-  <!-- Core theme CSS (Flowbite)-->
+  {{-- Core theme CSS (Flowbite) --}}
   <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css" rel="stylesheet" />
-  <!-- Jquery cdn -->
-  <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+  {{-- Jquery cdn --}}
+  <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+      crossorigin="anonymous"></script>
+  {{-- Font awsome cdn --}}
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+  {{-- CKEditor cdn --}}
+  <script src="https://cdn.ckeditor.com/ckeditor5/40.0.0/classic/ckeditor.js"></script>
 </head>
 
-<body class="min-h-screen">
-  <!-- Top navigation-->
-  @include('dashboard.layout.navbar')
+<body class="bg-gray-100 flex">
+  {{-- Sidebar --}}
+  @include('dashboard.layout.sidebar')
 
-    <div class="flex flex-row" id="wrapper">
-        <!-- Sidebar-->
-        @include('dashboard.layout.sidebar')
+  <div class="w-full flex flex-col h-screen overflow-y-hidden">
+    {{-- Top navigation --}}
+    @include('dashboard.layout.navbar')
+    
+    {{-- Page content --}}
+    @yield('content')
+    
 
-        <!-- Page content wrapper-->
-        <div id="page-content-wrapper" class="h-screen w-screen">
-
-            <!-- Page content-->
-            @yield('content')
-        </div>
-    </div>
-    <!-- Flowbite core JS-->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
-    <!-- Core theme JS-->
-    <script src="{{ asset('admin/js/scripts.js') }}"></script>
-    <script src="{{ secure_asset('admin/js/scripts.js') }}"></script>
+  </div>
+  <!-- AlpineJS -->
+  <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+  {{-- Flowbite core JS --}}
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+  {{-- Core theme JS --}}
+  <script src="{{ asset('admin/js/scripts.js') }}"></script>
+  <script src="{{ secure_asset('admin/js/scripts.js') }}"></script>
 </body>
 
 </html>
