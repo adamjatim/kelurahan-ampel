7c2b1feb6dabd5787b77e48af1f16ade0d0034ab
adamjatim <adamrahmatilahi@gmail.com>
2024-07-29 23:50:56 +0700

    refactor: Removed unused logic for loading and processing language data in the index method.

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -9,15 +9,9 @@ use Illuminate\Support\Facades\File;
 class AboutController extends Controller
 {
     public function index(){
-        $lang_url   = public_path('admin/devicon.json');
-        $lang_data  = file_get_contents($lang_url);
-        $lang_data  = json_decode($lang_data, true);
-        $lang       = array_column($lang_data, 'name');
-        $lang       = '"'. implode('","', $lang). '"';
 
         return view('dashboard.index')->with([
             'title' => 'About',
-            'source'=>  $lang
         ]);
     }
 
